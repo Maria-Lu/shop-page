dc382bf071563c7edfb980b5b0d878f6e58b32cb
Maria Poskrebysheva <m.s.poskrebysheva@yandex.ru>
2021-10-04 12:02:53 +0300

    Updates

---
+++ _nuxt/static/1633338166/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fshop-page\u002F_nuxt\u002Fstatic\u002F1633338166",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fshop-page\u002F",assetsPath:"\u002Fshop-page\u002F_nuxt\u002F",cdnURL:a}}}}(null));
